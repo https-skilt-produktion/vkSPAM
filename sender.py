982763510353875fbcb5c3e4816abec9180a3852
c4md <55578496+freethemesvk@users.noreply.github.com>
2022-02-05 13:57:47 +0300

    Update sender.py

--- sender.py
+++ sender.py
@@ -33,7 +33,18 @@ from time import sleep
 from datetime import datetime
 import traceback
 
-vk_session = vk_api.VkApi('+375292938793', '123321AAD')
+def captcha_handler(captcha):
+    """ При возникновении капчи вызывается эта функция и ей передается объект
+        капчи. Через метод get_url можно получить ссылку на изображение.
+        Через метод try_again можно попытаться отправить запрос с кодом капчи
+    """
+
+    key = input("Enter captcha code {0}: ".format(captcha.get_url())).strip()
+
+    # Пробуем снова отправить запрос с капчей
+    return captcha.try_again(key)
+
+vk_session = vk_api.VkApi('+375292938793', '123321AAD', captcha_handler=captcha_handler)
 vk_session.auth()
 
 vk = vk_session.get_api()
